89812c262babcafc97013f287b18fd5d6121603b
poojasharma20 <77521794+poojasharma20@users.noreply.github.com>
2021-02-20 10:09:36 +0530

    Add files via upload

---
+++ Algorithm Practical 8.py
@@ -0,0 +1,20 @@
+def issubsetsum(set,n,sum):
+    
+    if(sum == 0):
+        return True
+    if(n == 0 and sum !=0 ):
+        return False
+
+    if(set[n-1] > sum):
+        return issubsetsum(set,n-1,sum);
+    
+    return issubsetsum(set,n-1,sum) or issubsetsum(set,n-1,sum-set[n-1])
+
+set=[3,34,4,12,5,2]
+sum=9
+n=len(set)
+if(issubsetsum(set,n,sum)==True):
+       print("Found a subset with given sum")
+else:
+    print("No subset with given sum")
+    
